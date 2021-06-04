7443c5b8020bd8bb34fff8f978b4fa1371f04593
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2021-06-04 17:57:57 -0300

    Players service and create method

--- src/players/players.controller.ts
+++ src/players/players.controller.ts
@@ -1,18 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreatePlayerDto } from './dtos/create-player.dto';
+import { PlayersService } from './players.service';
 
 @Controller('api/v1/players')
 export class PlayersController {
+  constructor(private readonly playersService: PlayersService) {}
+
   @Post()
   async createOrUpdatePlayer(@Body() createPlayerDto: CreatePlayerDto) {
-    const { name, email, phoneNumber } = createPlayerDto;
-
-    return JSON.stringify({
-      newUser: {
-        nome: name,
-        email: email,
-        telefone: phoneNumber,
-      },
-    });
+    await this.playersService.createOrUpdatePlayer(createPlayerDto);
   }
 }
