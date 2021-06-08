7d3c88ac345007558817af2605c25a36208cff11
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2021-06-08 09:10:17 -0300

    MongoDB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
+// import config, { IConfig } from 'config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PlayersModule } from './players/players.module';
 
+// const dbConfig: IConfig = config.get('App.database');
+
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27028/smart-ranking', {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    PlayersModule,
+  ],
   controllers: [],
   providers: [],
 })
